872fcfb13e29f7f4bb5f76b7df9340004e07a733
Kontses <libys97@gmail.com>
2024-12-09 11:36:20 +0200

    base changed

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/slick-portfolio-svelte';
+const base = '/My-Personal-Site';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
