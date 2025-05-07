f657dc7901b0b0e0ead559c49a1ca4054d4ffe43
kontses <libys97@gmail.com>
2025-05-07 23:20:10 +0300

    unreal διόρθωση 2

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -71,7 +71,7 @@ export const items: Array<Project> = [
 		period: {
 			from: new Date(2023, 9, 3), to: new Date(2023, 10, 2)
 		},
-		skills: getSkills('unreal engine','lua'),
+		skills: getSkills('unreal','lua'),
 		type: 'Indie Game',
 		screenshots: [
 			{
