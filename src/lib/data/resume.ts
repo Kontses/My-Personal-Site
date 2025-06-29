4d688517476314b0088c7d9bea864f34448ceecf
kontses <libys97@gmail.com>
2025-06-29 20:45:59 +0300

    Προσθήκη ημερομηνίας τελευταίας τροποποίησης στο CV

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,5 @@
 export const data = './CV.pdf';
 
 export const title = 'Résumé';
+
+export const lastModified = '2025-06-29';
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { data, title } from '@data/resume';
+	import { data, title, lastModified } from '@data/resume';
 
 	import Chip from '$lib/components/Chip/Chip.svelte';
 	import CommonPage from '$lib/components/CommonPage.svelte';
@@ -8,9 +8,12 @@
 <CommonPage {title}>
 	<div class="resume">
 		{#if data}
-			<a href={data} download>
-				<Chip size={'1.25em'}>Download</Chip>
-			</a>
+			<div class="resume-content">
+				<a href={data} download>
+					<Chip size={'1.25em'}>Download</Chip>
+				</a>
+				<p class="last-edit-text">Last Edit: {lastModified}</p>
+			</div>
 		{:else}
 			<Chip>Ooops! no CV at the moment.</Chip>
 		{/if}
@@ -22,9 +25,21 @@
 		display: flex;
 		justify-content: center;
 		margin-top: 20px;
+	}
+
+	.resume-content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 25px;
 
 		& > a {
 			color: inherit;
+		}
+
+		.last-edit-text {
+			font-size: 0.9em;
+			color: #aaa;
 		}
 	}
 </style>
