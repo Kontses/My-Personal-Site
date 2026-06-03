5d7fa38fa605e177bff956bd7d3f38db9387981c
kontses <libys97@gmail.com>
2026-06-03 21:39:04 +0300

    Set base path for Vercel deployment in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/My-Personal-Site';
+const base = process.env.VERCEL ? '' : '/My-Personal-Site';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
